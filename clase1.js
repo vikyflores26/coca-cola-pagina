36e8b3830b216015c15b99f2c792aa0679327734
alumno <alumno@Instituto.local>
2023-12-01 09:30:33 -0300

    cambios2

--- clase1.js
+++ clase1.js
@@ -1,13 +1,15 @@
 let cocazero = document.querySelector('#cocazero')
-let cocaImagen = document.querySelector('.coca')
+let cocaImagen = document.querySelector('#coca')
 
 let imagenCambiada = false
 cocazero.onclick = function () {
     if (imagenCambiada == false) {
-        cocaImagen.src = "Coca Zero.png"
+        cocaImagen.src = "coca Zero.png"
         imagenCambiada = true
     }else{
         cocaImagen.src = "camilo.png"
         imagenCambiada = false
     }
+
+    
 }
